bb1e292c73c4170aa7c3a073e8d041d57daa2fe3
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-05-09 14:09:55 +0000

    [Add]会員側のトップページ、アバウトページ、ログインページ、新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  namespace :admin do
+  end
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  scope module: :public do
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
