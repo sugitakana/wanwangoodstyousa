23ffc186dc461efc3da5d9ace0f9fd2e5e59e316
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-05-16 13:51:53 +0000

    [Fix]いいね機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,13 +2,15 @@ class Public::FavoritesController < ApplicationController
   before_action :set_review, only: [:create, :destroy]
   
   def create
-    @favorite = Favorite.new(customer_id: current_customer.id, review_id: params[:review_id])
-    @favorite.save
+    @review = Review.find(params[:id])
+    favorite = @review.favorites.new(customer_id: current_customer.id)
+    favorite.save
   end
   
   def destroy
-    @favorite = Favorite.find_by(customer_id: current_customer.id, review_id: params[:review_id])
-    @favorite.destroy
+    @review = Review.find(params[:id])
+    favorite = current_customer.favorites.find_by(review_id: @review.id)
+    favorite.destroy
   end
   
   private
