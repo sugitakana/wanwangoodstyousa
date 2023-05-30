56e4db9c77eeeb82af6a19d56f8438fb95f56949
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-05-30 04:51:45 +0000

    [Add]レイアウト、長い文字省略するように

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,32 @@ Admin.create!(
   email: 'admin@admin',
   password: 'admintest'
 )
+
+Customer.create!(
+  [
+    {name: '太郎', email: 'taro@example.com', password: 'password'},
+    {name: '花子', email: 'hanako@example.com', password: 'password'},
+    {name: '山田', email: 'yamada@example.com', password: 'password'}
+  ] 
+)
+
+Tag.create!(
+  [
+    {name: 'フード・おやつ'},
+    {name: 'ベッド'},
+    {name: '首輪・ハーネス・リード'},
+    {name: 'おもちゃ'},
+    {name: '洋服'},
+    {name: 'ケア用品'},
+    {name: 'その他'}
+  ]
+)
+
+Review.create!(
+  [
+    {item_name: 'ワンワンビスケット', item_price: '450', dog_breed: '柴犬', tag_id: 1, review: 'とっても気に入ったようで美味しそうに食べています！', rate: '5', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.JPG"), filename:"sample-post1.JPG"), customer_id: 1},
+    {item_name: '水のいらない泡シャンプー', item_price: '600', dog_breed: 'ダックスフンド', tag_id: 6, review: '臭いが気になったので購入しました。いい感じです。', rate: '4', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.JPG"), filename:"sample-post2.JPG"), customer_id: 2},
+    {item_name: 'ササミ&野菜ジャーキーフード', item_price: '650', dog_breed: 'トイプードル', tag_id: 1, review: '好きみたいです！', rate: '5', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.JPG"), filename:"sample-post3.JPG"), customer_id: 3},
+    {item_name: 'チューイングスティック 超小型犬用', item_price: '440', dog_breed: 'チワワ', tag_id: 1, review: '食後にあげてます。喜んで食べます。', rate: '5', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.JPG"), filename:"sample-post4.JPG"), customer_id: 1}
+  ]
+)
