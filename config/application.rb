41fb0caea48980ff47ce7881e776632d25fc76c3
sugitakana <126451068+sugitakana@users.noreply.github.com>
2023-06-29 11:28:51 +0000

    [Add]レビュー一覧、レビュー詳細に投稿時間を表示

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Wanwangoodstyousa
     
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
